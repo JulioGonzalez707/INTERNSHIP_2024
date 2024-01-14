d5da33fc5472bf384377c37e1a256a4127a47aa7
Julio González <143663235+JulioGonzalez707@users.noreply.github.com>
2024-01-14 13:32:05 -0600

    ACTIVITIES

---
+++ CW_1/CW_1_Activity_7/SC_extern_exercise/SC_extern_exercise.h
@@ -0,0 +1,15 @@
+
+/*
+ * SC_extern_exercise.h
+ *
+ *  Created on: Jan 8, 2024
+ *      Author: julio
+ */
+
+#ifndef SC_EXTERN_EXERCISE_H_
+#define SC_EXTERN_EXERCISE_H_
+
+int x = 14;
+
+
+#endif /* SC_EXTERN_EXERCISE_H_ */
